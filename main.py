c2bb467c015c7b200c94e56522a993c06acd5a9a
Andres Lopez <ing.andlopgam5@gmail.com>
2025-07-07 20:15:15 -0700

    Added structure and clearing console function to main file

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 # IMPORTS
+import pandas as pd
 from utils.menu import *
-
-# INITIAL DATA STRUCTURES AND VARIABLES
-
+from utils.import_save_csv import *
+import os
 
 # FUNCTIONS
+def clear_console():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
 
+# INITIAL DATA STRUCTURES AND VARIABLES
+user_name = input("Enter your name: ")
+clear_console()
+print(f"Welcome {user_name}.")
+df = open_csv()
 
 # MAIN STRUCTURE
 x = main_menu_choice()
 display_second_tier_menu(x)
-second_tier_menu_choice(x)
+y = second_tier_menu_choice(x)
