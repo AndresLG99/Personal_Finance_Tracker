f4d18e44158eb0fd45ef78a982ed8065dad6ada9
Andres Lopez <ing.andlopgam5@gmail.com>
2025-07-08 00:19:50 -0700

    Initial functions for data management

---
+++ utils/data_management.py
@@ -0,0 +1,67 @@
+# IMPORTS
+import pandas as pd
+import os
+
+# INITIAL DATA STRUCTURES AND VARIABLES
+main_menu_options = ["Import",
+                     "Show",
+                     "Modify",
+                     "Analyze",
+                     "Visualize",
+                     "Save",
+                     "Exit"]
+
+show_menu_options = ["View all transactions",
+                     "View transactions by date range"]
+
+modify_menu_options = ["Add a transaction",
+                       "Edit a transaction",
+                       "Delete a transaction"]
+
+analyze_menu_options = ["Analyze spending by category",
+                         "Analyze average monthly spending",
+                         "Analyze top spending category"]
+
+directory = "C:\\Users\\andre\\Documents\\Data Science\\DS-201\\Final Project\\Code\\data"
+
+# FUNCTIONS
+def display_menu(menu):
+    for index, option in enumerate(menu):
+        print(f"{index + 1}. {option}")
+
+def display_files():
+    for index, file in enumerate(os.listdir(directory)):
+        if os.path.isfile(os.path.join(directory, file)):
+            print(f"{index + 1}. {file}")
+
+def open_csv(filename):
+    df = pd.read_csv(f"{directory}\\{filename}")
+    return df
+
+def show_all_transactions(df):
+    print(df)
+
+def show_filtered_transactions(df, init_date, end_date):
+    df = df[df["Date"] >= init_date]
+    df = df[df["Date"] <= end_date]
+    return df
+
+def add_transaction(df, param1, param2, param3, param4, param5):
+    new_transaction = {"Date": param1,
+                       "Category": param2,
+                       "Description": param3,
+                       "Amount": param4,
+                       "Type": param5}
+    df.append(new_transaction)
+    return df
+
+def edit_transaction(df, row, column, new_value):
+    df.loc[row, column] = new_value
+    return df
+
+def delete_transaction(df, index):
+    df = df.drop([index, index + 1])
+    return df
+
+def save_csv(df, filename):
+    df.to_csv(filename, index=False)
