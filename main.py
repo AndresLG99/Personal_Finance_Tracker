58d50850a7aa730dcd6d6d394bd0169efda143be
Andres Lopez <ing.andlopgam5@gmail.com>
2025-07-07 23:02:36 -0700

    Start from scratch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # IMPORTS
-from utils.menu import *
+
 
 # INITIAL DATA STRUCTURES AND VARIABLES
 
@@ -8,6 +8,3 @@ from utils.menu import *
 
 
 # MAIN STRUCTURE
-x = main_menu_choice()
-display_second_tier_menu(x)
-second_tier_menu_choice(x)
